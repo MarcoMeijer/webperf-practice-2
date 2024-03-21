f8e4613980be1607636f68876a50c40710a3a697
Marco Meijer <marcomeijerm@gmail.com>
2024-03-21 12:17:01 +0100

    feat: added other pages to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: []
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        contact: resolve(__dirname, "contact.html"),
+        menu: resolve(__dirname, "menu.html"),
+        newsdetail: resolve(__dirname, "news-detail.html"),
+        news: resolve(__dirname, "news.html"),
+      }
+    }
+  },
 });
