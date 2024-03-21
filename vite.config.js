546db3eb6e85ffbfee51d1c464e07ff17f2a528e
Marco Meijer <marcomeijerm@gmail.com>
2024-03-21 12:26:07 +0100

    feat: include js in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
+        bootstrapIcons: resolve(__dirname, "css/bootstrap-icons.css"),
+        bootstrapMin: resolve(__dirname, "css/bootstrap.min.css"),
+        tooplate: resolve(__dirname, "css/tooplate-crispy-kitchen.css"),
+        bootstrap: resolve(__dirname, "js/bootstrap.bundle.min.js"),
+        custom: resolve(__dirname, "js/custom.js"),
+        jquery: resolve(__dirname, "js/jquery.min.js"),
         main: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"),
         contact: resolve(__dirname, "contact.html"),
