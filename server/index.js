327522548ac148c19f3d632648a4ee05768ef72d
Marco Meijer <marcomeijerm@gmail.com>
2024-03-25 11:45:45 +0100

    feat: endpoint for reservation form

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const compression = require('compression');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('../dist'));
 app.set('etag', 'strong');
 
@@ -11,3 +14,27 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
+app.post('/book', (req, res) => {
+  const { name, date, time } = req.body;
+
+  fs.writeFileSync(`./data/${name} ${date} ${time}PM ${makeid(5)}.json`, JSON.stringify(req.body));
+
+  // redirect user back
+  res.writeHead(302, {
+    'Location': '/'
+  });
+  res.end();
+});
