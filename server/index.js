a133346ab6f474fda8f3151894fd31bdd3d7cc2d
Marco Meijer <marcomeijerm@gmail.com>
2024-03-25 11:22:32 +0100

    feat: set etag to strong

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const port = 3000;
 
 app.use(compression());
 app.use(express.static('../dist'));
+app.set('etag', 'strong');
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
