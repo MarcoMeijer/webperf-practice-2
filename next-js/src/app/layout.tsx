abb0b57e5b9822f8c07812c594fda8ca6a3aeb1b
Marco Meijer <marcomeijerm@gmail.com>
2024-03-27 10:54:16 +0100

    feat: add home page to next-js

--- next-js/src/app/layout.tsx
+++ next-js/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./bootstrap-icons.css";
+import "./bootstrap.min.css";
+import "./tooplate-crispy-kitchen.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
