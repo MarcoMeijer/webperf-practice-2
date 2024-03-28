d69ae3638f7e7092789d58858d7b223f17fb4092
Marco Meijer <marcomeijerm@gmail.com>
2024-03-28 17:29:18 +0100

    feat: skeleton components for cocktails

--- next-js/src/pages/cocktails.tsx
+++ next-js/src/pages/cocktails.tsx
@@ -3,16 +3,22 @@ import { useEffect, useState } from "react";
 import { Skeleton } from "@nextui-org/react";
 import List from 'react-virtualized/dist/commonjs/List';
 
+function timeout(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export default function Cocktails() {
   const [search, setSearch] = useState("");
   const [data, setData] = useState<null | any>(null);
-
-  console.log({ data });
 
   useEffect(() => {
     setData(null);
     let cancelled = false;
     const f = async () => {
+      await timeout(1000); // add some artificial timeout to view skeleton components
+      if (cancelled) {
+        return;
+      }
       const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`);
       const json = await response.json();
       if (!cancelled) {
@@ -28,32 +34,52 @@ export default function Cocktails() {
 
   const isLoaded = data !== null;
 
-  const rowHeight = 240;
+  const rowCount = isLoaded ? data.drinks?.length ?? 0 : 25;
+
+  const rowHeight = 248;
 
   const renderCocktail = ({ index, key, style }) => {
+    if (!isLoaded) {
+      return (<div key={key} className="col-lg-4 col-md-6 col-12" style={style}>
+        <div className="flex flex-row p-4">
+          <Skeleton className="rounded-lg" style={{ height: rowHeight - 8, width: rowHeight - 8 }} />
+
+          <div className="flex flex-col">
+            <Skeleton className="rounded-full w-96 mx-1 my-2">
+              <h4 className="mb-0">Drink name</h4>
+            </Skeleton>
+            <Skeleton className="mb-0 text-neutral-500 w-48 text-lg rounded-full m-1">Ingredients:</Skeleton>
+            <Skeleton className="rounded-full w-40 m-1"><p>First ingredient</p></Skeleton>
+            <Skeleton className="rounded-full w-48 m-1"><p>Second ingredient</p></Skeleton>
+            <Skeleton className="rounded-full w-36 m-1"><p>Last</p></Skeleton>
+          </div>
+        </div>
+      </div>);
+    }
+
     const drink = data.drinks[index];
     return (<div key={key} className="col-lg-4 col-md-6 col-12" style={style}>
-      <div className="flex flex-row">
-        <img src={drink.strDrinkThumb} className="img-fluid menu-image" alt="" height={rowHeight} width={rowHeight} />
+      <div className="flex flex-row p-4">
+        <img src={drink.strDrinkThumb} className="img-fluid menu-image" alt="" height={rowHeight - 8} width={rowHeight - 8} />
 
-        <div className="flex flex-col">
-          <h4 className="mb-0">{drink.strDrink}</h4>
-          <h5 className="mb-0 text-neutral-500 text-lg">Ingredients:</h5>
-          <p>{drink.strIngredient1}</p>
-          <p>{drink.strIngredient2}</p>
-          <p>{drink.strIngredient3}</p>
-          <p>{drink.strIngredient4}</p>
-          <p>{drink.strIngredient5}</p>
-          <p>{drink.strIngredient6}</p>
-          <p>{drink.strIngredient7}</p>
-          <p>{drink.strIngredient8}</p>
-          <p>{drink.strIngredient9}</p>
-          <p>{drink.strIngredient10}</p>
-          <p>{drink.strIngredient11}</p>
-          <p>{drink.strIngredient12}</p>
-          <p>{drink.strIngredient13}</p>
-          <p>{drink.strIngredient14}</p>
-          <p>{drink.strIngredient15}</p>
+        <div className="flex flex-col m-1">
+          <h4 className="m-1">{drink.strDrink}</h4>
+          <h5 className="m-1 text-neutral-500 text-lg">Ingredients:</h5>
+          <p className="mx-1">{drink.strIngredient1}</p>
+          <p className="mx-1">{drink.strIngredient2}</p>
+          <p className="mx-1">{drink.strIngredient3}</p>
+          <p className="mx-1">{drink.strIngredient4}</p>
+          <p className="mx-1">{drink.strIngredient5}</p>
+          <p className="mx-1">{drink.strIngredient6}</p>
+          <p className="mx-1">{drink.strIngredient7}</p>
+          <p className="mx-1">{drink.strIngredient8}</p>
+          <p className="mx-1">{drink.strIngredient9}</p>
+          <p className="mx-1">{drink.strIngredient10}</p>
+          <p className="mx-1">{drink.strIngredient11}</p>
+          <p className="mx-1">{drink.strIngredient12}</p>
+          <p className="mx-1">{drink.strIngredient13}</p>
+          <p className="mx-1">{drink.strIngredient14}</p>
+          <p className="mx-1">{drink.strIngredient15}</p>
         </div>
       </div>
     </div>);
@@ -133,7 +159,7 @@ export default function Cocktails() {
 
             <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
 
-            <List height={600} width={1200} rowHeight={rowHeight} rowCount={isLoaded ? data.drinks?.length ?? 0 : 0} rowRenderer={renderCocktail} />
+            <List height={600} width={1200} rowHeight={rowHeight} rowCount={rowCount} rowRenderer={renderCocktail} />
           </div>
         </div>
       </section>
