63f09914647d2d100749ddefd2490b3278dd0281
Marco Meijer <marcomeijerm@gmail.com>
2024-03-28 18:51:51 +0100

    feat: error pages

---
+++ next-js/src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function NotFound() {
+  return <main style={{ width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+    <p className="text-gray-900"><b>404</b> - Page doesn't exist 🤨</p>
+  </main>
+}
